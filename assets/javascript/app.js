e4cb3fcae3b748ec61fde8652bd17ffee111258b
Jose De Las Salas <SueBuSo@Joses-MBP.attlocal.net>
2018-06-10 11:52:42 -0400

    gave functionality to start btn, got it present text upon pressing, will begin updating function of button to do other things.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,15 @@
 //establishing a function for the start button.
 
-document.ready(function() {
-("#button").click(function() {
+$(document).ready(function() {
 
-});
+   var startBtn = "<h2>Testing...</h2>";
+
+   function initialize () {
+    $("#game").append(startBtn);
+}
+    
+    $("#startBtn").click(function() {
+        initialize();
+    });
+   
 })
